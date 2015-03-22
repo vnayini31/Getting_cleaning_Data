c5816c267adc403803ea24b64af09f9bf1c30d47
vnayini31 <viveknayini@Viveks-MacBook-Air.local>
2015-03-22 13:43:26 -0700

    test

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,13 @@
-UCI HAR Dataset/train/.DS_Store
+if (!require("data.table")) {
+    install.packages("data.table")
+}
+
+if (!require("reshape2")) {
+    install.packages("reshape2")
+}
+
+require("data.table")
+require("reshape2")
+
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
