daf48d4c1a883bf7cd6065103bc65f1b1af91242
vnayini31 <viveknayini@Viveks-MacBook-Air.local>
2015-03-22 12:34:58 -0700

    run_analysis.R
    empty code

---
+++ quiz2_1.R
@@ -0,0 +1,22 @@
+library(httr)
+require(httpuv)
+require(jsonlite)
+
+# 1. Find OAuth settings for github:
+#    http://developer.github.com/v3/oauth/
+oauth_endpoints("github")
+
+# 2. Register an application at https://github.com/settings/applications
+#    Insert your values below - if secret is omitted, it will look it up in
+#    the GITHUB_CONSUMER_SECRET environmental variable.
+#
+#    Use http://localhost:1410 as the callback url
+myapp <- oauth_app("v9e", "759141d17ca1c63265c4", secret="df7874afc6977f0afbabdbafb35390fab91af41a")
+
+# 3. Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# 4. Use API
+req <- GET("https://api.github.com/users/jtleek/repos", config(token = github_token))
+stop_for_status(req)
+output <- content(req)
